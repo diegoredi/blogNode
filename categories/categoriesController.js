c1d1398c9ffa5906c3a5c2d74b8065af8afff97b
diegoredi <contato.diegoredi@gmail.com>
2022-05-05 00:21:11 -0300

    Edição finalizada

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -50,10 +50,14 @@ router.post("/admin/categories/delete", (req, res) =>{
 router.get("/admin/categories/edit/:id", (req, res) => {
     var id = req.params.id;
 
-    Category.findByPk(id).then(categoria => {
-        if(categoria != undefined){
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
 
-            res.render("/admin/categories/edit.ejs", {categoria: categoria});
+    Category.findByPk(id).then(category => {
+        if(category != undefined){
+
+            res.render("admin/categories/edit.ejs", {category: category});
 
         }else{
             res.redirect("/admin/categories");
@@ -61,6 +65,25 @@ router.get("/admin/categories/edit/:id", (req, res) => {
     }).catch(erro => {
         res.redirect("/admin/categories");
     })
+});
+
+router.post("/categories/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+
+    Category.update({
+            title: title,
+            slug: slugify(title)
+        },
+        {
+            where: {
+                id: id
+            }
+        }).then(() => {
+            res.redirect("/admin/categories");
+        })
+
+
 });
 
 
